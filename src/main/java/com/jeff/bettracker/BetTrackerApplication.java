660458c26e7db643dc154015c61d8f0b4741f3f6
Jeffrey949 <Jeffrey.boateng94@gmail.com>
2025-01-03 16:44:07 +0100

    innitial commit

---
+++ src/main/java/com/jeff/bettracker/BetTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.jeff.bettracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jeff/bettracker/BetTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeff.bettracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
